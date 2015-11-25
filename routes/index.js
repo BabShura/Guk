70e17b1b29a577456c0fc587603ead5a33c6d007
karloluis <karlo.luis.m@gmail.com>
2015-11-25 04:47:00 -0400

    Found typo for trigger filter.

--- routes/index.js
+++ routes/index.js
@@ -8,25 +8,31 @@ router.route('/')
 
 router.route('/slack')
 .post( (req, res, next) => {
-    trigger = req.body.tirgger_word
+    trigger = req.body.trigger_word
     command = req.body.text
     command.replace(trigger ,'').trim().split(' ') //condense the call to an array of strings
-    console.log(command)
 
-    if(command[0].toLowerCase() == "champion" || command[0].toLowerCase() == "champ"){
-            text: "Testing champ"
-    }
-    else if (command[0].toLowerCase() == "monster") {
-            text: "Testing monster"
-    }
-    else if (command[0].toLowerCase() == "map") {
+    if(command[0].toLowerCase() === "champion" || command[0].toLowerCase() === "champ"){
         var saybot = {
-            text: "Testing map"
+            text: "Testing champion"
         }
 
     }
-    else if (command[0].toLowerCase() == "item") {
+    else if (command[0].toLowerCase() === "monster") {
+        var saybot = {
+            text: "Testing monster"
+        }
+
+    }
+    else if (command[0].toLowerCase() === "map") {
+        var saybot = {
+            text: "Testing map"
+        }
+    }
+    else if (command[0].toLowerCase() === "item") {
+        var saybot = {
             text: "Testing item"
+        }
     }
     else {
         var saybot = slack.respond(req.body, (hook)=>{
