42ce2c56a6288bf83973ae6a23bfb652abe2e8d3
karloluis <karlo.luis.m@gmail.com>
2015-11-25 19:48:52 -0400

    Fixed indentation.

--- League/champ.js
+++ League/champ.js
@@ -1,62 +1,61 @@
-console.log("IN CHAMP JS", command)
-    var name = command[1];
-    var skin = command[2] || '0';
-    league_v = http.get("https://ddragon.leagueoflegends.com/realms/na.json")
-    console.log(league_v)
+var name = command[1];
+var skin = command[2] || '0';
+league_v = http.get("https://ddragon.leagueoflegends.com/realms/na.json")
+console.log(league_v)
 
-    champ = http.get("http://ddragon.leagueoflegends.com/cdn/" + league_v + "/data/en_US/champion/" + name + ".json")
+champ = http.get("http://ddragon.leagueoflegends.com/cdn/" + league_v + "/data/en_US/champion/" + name + ".json")
 
-    champ.img.links = {
-        "load" : "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + champ.name + "_" + champ_skin + ".jpg",
-        "square" : "http://ddragon.leagueoflegends.com/cdn/" + league_v.champion + "/img/champion/" + champ.img.full ,
-        "sprite" : "http://ddragon.leagueoflegends.com/cdn/" + leagu_v.champion + "/img/sprite/"
-    }
-
-
-    //Make attachments for each data set
-
-    // {
-        //     "fallback": "Required plain-text summary of the attachment.",
-        //     "color": "#36a64f",
-        //     "pretext": "Optional text that appears above the attachment block",
-        //     "author_name": "Bobby Tables",
-        //     "author_link": "http://flickr.com/bobby/",
-        //     "author_icon": "http://flickr.com/icons/bobby.jpg",
-        //     "title": "Slack API Documentation",
-        //     "title_link": "https://api.slack.com/",
-        //     "text": "Optional text that appears within the attachment",
-        //     "fields": [
-        //         {
-        //             "title": "Priority",
-        //             "value": "High",
-        //             "short": false
-        //         }
-        //     ],
-        //     "image_url": "http://my-website.com/path/to/image.jpg",
-        //     "thumb_url": "http://example.com/path/to/thumb.png"
-        // }
-
-        basic_info = {
-            "fallback": "Basic champ information",
-            "title": "Basic information",
-            "text": ""
-
-        }
-
-        spells_info = {
-            "fallback": "Champ Spells",
-            "title": "Spells",
-            "text": ""
-
-        }
+champ.img.links = {
+    "load" : "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + champ.name + "_" + champ_skin + ".jpg",
+    "square" : "http://ddragon.leagueoflegends.com/cdn/" + league_v.champion + "/img/champion/" + champ.img.full ,
+    "sprite" : "http://ddragon.leagueoflegends.com/cdn/" + leagu_v.champion + "/img/sprite/"
+}
 
 
-    champ_data = {
-        "text": "Champ Info",
-        "username": "LoL-champ",
-        "icon_emoji": ":champ:"
-        // "attachments": []
-    }
+//Make attachments for each data set
+
+// {
+//     "fallback": "Required plain-text summary of the attachment.",
+//     "color": "#36a64f",
+//     "pretext": "Optional text that appears above the attachment block",
+//     "author_name": "Bobby Tables",
+//     "author_link": "http://flickr.com/bobby/",
+//     "author_icon": "http://flickr.com/icons/bobby.jpg",
+//     "title": "Slack API Documentation",
+//     "title_link": "https://api.slack.com/",
+//     "text": "Optional text that appears within the attachment",
+//     "fields": [
+//         {
+//             "title": "Priority",
+//             "value": "High",
+//             "short": false
+//         }
+//     ],
+//     "image_url": "http://my-website.com/path/to/image.jpg",
+//     "thumb_url": "http://example.com/path/to/thumb.png"
+// }
+
+basic_info = {
+}
+    "fallback": "Basic champ information",
+    "title": "Basic information",
+    "text": ""
+
+
+spells_info = {
+    "fallback": "Champ Spells",
+    "title": "Spells",
+    "text": ""
+
+}
+
+
+champ_data = {
+    "text": "Champ Info",
+    "username": "LoL-champ",
+    "icon_emoji": ":champ:"
+    // "attachments": []
+}
 
 module.exports = {
     champ : champ_data
