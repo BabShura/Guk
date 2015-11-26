cdcc1c356f9ef75d0c2b54454e7b8a078cd9bf30
karloluis <karlo.luis.m@gmail.com>
2015-11-26 14:08:50 -0400

    Fix minor bugs.

--- League/champ.js
+++ League/champ.js
@@ -6,8 +6,8 @@ var name = command[1];
 var skin = command[2] || '0';
 
 //Access ddragon latest version for Champions
-console.log(league.versions.res)
-champ_v = league.versions.res.info.n.champion
+console.log(league.versions)
+champ_v = league.versions.info.n.champion
 
 
 champ = http.get("http://ddragon.leagueoflegends.com/cdn/" + champ_v + "/data/en_US/champion/" + name + ".json", (res) => {
