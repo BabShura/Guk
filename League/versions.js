0d48ef3f42f66ccdcdba8f32f3cf9b47c6b759b6
karloluis <karlo.luis.m@gmail.com>
2015-11-27 07:17:56 -0400

    Fix minor bugs.

--- League/versions.js
+++ League/versions.js
@@ -1,27 +1,24 @@
 var https = require('https');
 
-exports.versions = () => {
-    data = {}
+data = {}
 
-    return setTimeout( (data)=>{
-        console.log("Check out the version", data)
-        return data;
+https.get("https://ddragon.leagueoflegends.com/realms/na.json", (res) => {
+    info = ""
+    res.setEncoding('utf8')
+    res.on('data', (chunk) => {
+        info += chunk;
     })
+    res.on('end', () => {
+        try {
+            data = JSON.parse(info);
+            console.log("INSIDE", data)
+            // expose this value, used by other modules to retrieve data.
+        } catch (err) {
+            console.error('Unable to parse response as JSON', err);
+        }
+    })
+})
 
-    https.get("https://ddragon.leagueoflegends.com/realms/na.json", (res) => {
-        info = ""
-        res.setEncoding('utf8')
-        res.on('data', (chunk) => {
-            info += chunk;
-        })
-        res.on('end', () => {
-            try {
-                data = JSON.parse(info);
-                console.log("INSIDE", data)
-                // expose this value, used by other modules to retrieve data.
-            } catch (err) {
-                console.error('Unable to parse response as JSON', err);
-            }
-        })
-    })
-}
+return setTimeout( ()=>{
+    console.log("CHECK", data)
+})
