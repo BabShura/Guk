8082636dadb0160cb61de35e4d5ce42cac4a49a4
karloluis <karlo.luis.m@gmail.com>
2015-11-27 16:50:09 -0400

    Test

--- League/versions.js
+++ League/versions.js
@@ -24,5 +24,5 @@ https.get("https://ddragon.leagueoflegends.com/realms/na.json",(res)=>{
             console.error('Unable to parse response as JSON', err);
         }
     })
-    setTimeout(()=>{ console.log(d.value); exports.version = d.value})
+    setTimeout(()=>{ console.log(d.value); exports.version = d.value ; console.log(exports.version);})
 })
