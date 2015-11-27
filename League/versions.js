ad734001722ba4b9b2b07119aa05fcf1b404dc60
karloluis <karlo.luis.m@gmail.com>
2015-11-27 16:55:24 -0400

    Find fix

--- League/versions.js
+++ League/versions.js
@@ -24,5 +24,7 @@ https.get("https://ddragon.leagueoflegends.com/realms/na.json",(res)=>{
             console.error('Unable to parse response as JSON', err);
         }
     })
-    setTimeout(()=>{ console.log(d.value); exports.version = d.value ; console.log(exports.version);})
+    setTimeout(()=>{ exports.version = d.value ; console.log(exports.version);})
 })
+
+    setTimeout(()=>{ console.log("VERSIONS AFTER"); console.log(exports.version);})
