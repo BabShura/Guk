4dc6e5e7c3e0d60f7da6daba86ffa0c1a71f1037
karloluis <karlo.luis.m@gmail.com>
2015-11-27 06:46:41 -0400

    Use timeOut to assure values are set before returning

--- League/versions.js
+++ League/versions.js
@@ -1,18 +1,24 @@
 var https = require('https');
 
+exports.versions = () => {
+    versions = {}
+    https.get("https://ddragon.leagueoflegends.com/realms/na.json", (res) => {
+        data = ""
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+            data += chunk;
+        })
+        res.on('end', () => {
+            try {
+                versions = JSON.parse(data);
+                // expose this value, used by other modules to retrieve data.
+            } catch (err) {
+                console.error('Unable to parse response as JSON', err);
+            }
+        })
+    })
 
-https.get("https://ddragon.leagueoflegends.com/realms/na.json", (res) => {
-    data = ""
-    res.setEncoding('utf8')
-    res.on('data', (chunk) => {
-        data += chunk;
+    setTimeOut( ()=>{
+     return versions
     })
-    res.on('end', () => {
-        try {
-            exports.versions = JSON.parse(data);
-            // expose this value
-        } catch (err) {
-            console.error('Unable to parse response as JSON', err);
-        }
-    })
-})
+}
