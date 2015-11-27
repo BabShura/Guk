f8e0c619a02a54d62d73ae24588a29c8d5356dcf
karloluis <karlo.luis.m@gmail.com>
2015-11-27 09:45:30 -0400

    Find Fix.

--- League/versions.js
+++ League/versions.js
@@ -4,39 +4,30 @@ function version(){
     this.data = null;
 };
 
-version.prototype.export = function(info) {
-    this.data = info;
-};
-
-version.prototype.retrieve = function() {
-    return this.data;
-};
-
-function objectchanger(obj,info)
-{
-	obj.export(info); // runs the method of the object being passed in
-}
-
-v = new version();
-
-
-https.get("https://ddragon.leagueoflegends.com/realms/na.json", (res, objectchanger) => {
-    info = ""
+version.prototype.set = function(res) {
     res.setEncoding('utf8')
     res.on('data', (chunk) => {
         info += chunk;
     })
     res.on('end', () => {
         try {
-            data = JSON.parse(info); //has data
-            objectchanger(v,data)
+            this.data = JSON.parse(info); //has data
             // expose this value, used by other modules to retrieve data.
         } catch (err) {
             console.error('Unable to parse response as JSON', err);
         }
     })
-})
+};
 
-console.log("v_obj", v);
+version.prototype.retrieve = function() {
+    return this.data;
+};
+
+v = new version();
+
+
+https.get("https://ddragon.leagueoflegends.com/realms/na.json", v.set)
+
+console.log("v_obj CHECK", v);
 
 exports.versions = v.retrieve();
