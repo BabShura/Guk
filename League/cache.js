405ced8b9306c6646766e18fae0b4c5e647e3026
karloluis <karlo.luis.m@gmail.com>
2015-12-23 10:56:59 -0400

    Try caching.

--- League/cache.js
+++ League/cache.js
@@ -1,5 +1,5 @@
 module.exports = function(api){
-    this.cache = {}
+    this.cache = {items:{}, champs:{}}
     var that = this;
 
     opt = {
@@ -10,11 +10,9 @@ module.exports = function(api){
 
     api.getItemData(opt).then((res)=>{
         that.cache.items = res;
-        console.log(res);
     })
     api.getChampionData(opt).then((res)=>{
         that.cache.champs = res;
-        console.log(res);
     })
 
     return this.cache
