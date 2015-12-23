28425247495e06951466610d8418e7f11b55ce0f
karloluis <karlo.luis.m@gmail.com>
2015-12-23 10:54:23 -0400

    Fix bugs.

--- League/cache.js
+++ League/cache.js
@@ -10,9 +10,11 @@ module.exports = function(api){
 
     api.getItemData(opt).then((res)=>{
         that.cache.items = res;
+        console.log(res);
     })
     api.getChampionData(opt).then((res)=>{
         that.cache.champs = res;
+        console.log(res);
     })
 
     return this.cache
